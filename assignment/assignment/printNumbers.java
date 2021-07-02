0ba45d936e2a94a14cd288c727d61a58bf00af1f
Subhadarsini Pattnaik <71827873+Subhadarsini-10@users.noreply.github.com>
2021-07-02 19:58:58 +0530

    Add files via upload

---
+++ assignment/assignment/printNumbers.java
@@ -0,0 +1,17 @@
+package assignment;
+
+public class printNumbers {
+
+    public static void printNumber(int n){
+        if(n <= 0){
+            return;
+        }
+        printNumber(n-1);
+        System.out.println(n + " ");
+    }
+    public static void main(String[] args) {
+        int n = 6;
+        printNumber(n);
+        
+    }
+}
