716b09ed8daa7f881e5ee8bbad43036b9be1e76b
Subhadarsini Pattnaik <71827873+Subhadarsini-10@users.noreply.github.com>
2021-07-03 17:38:07 +0530

    Add files via upload

--- code/fibonacciNumber.java
+++ code/fibonacciNumber.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
 public class fibonacciNumber {
 
     public static int fib(int n){
+        
         if(n == 0 || n == 1){
             return n;
         }
         return fib(n-1) + fib(n-2);
     }
     public static void main(String[] args) {
-        int n = 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
         System.out.println(fib(n));
     }
 }
